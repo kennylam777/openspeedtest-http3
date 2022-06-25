65cdfb65b09dc3adf6fd23973d28e98f119e351a
Kenny Lam <5083773+kennylam777@users.noreply.github.com>
2022-06-25 06:47:47 +0100

    Improving documentations

--- README.md
+++ README.md
@@ -3,3 +3,12 @@
 This docker image is built for testing HTTP/3 performece on Nginx + Cloudflare quiche.
 
 
+## Example
+Launch with `http3_cc_algorithm=reno`, mounted with custom SSL certificate files.
+```
+docker run --name nginx --rm -ti -e QUIC_CC=reno -p 443:443 -p 443:443/udp -v $PWD/ssl:/etc/ssl kennylam777/openspeedtest-http3
+```
+
+## Known Issues
+1. Upload test may not work in HTTP/3, same issue as in HTTP/2
+2. Some cloud service provider may throttle UDP traffic, resulting slow performance
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
-
 fallocate -l 30M /usr/share/nginx/html/downloading
 
 sed -i "s/{HTTP_PORT}/${HTTP_PORT}/g" /etc/nginx/conf.d/OpenSpeedTest-Server-HTTP3.conf
@@ -8,6 +7,6 @@ sed -i "s/{HTTPS_PORT}/${HTTPS_PORT}/g" /etc/nginx/conf.d/OpenSpeedTest-Server-H
 sed -i "s/{QUIC_PORT}/${QUIC_PORT}/g" /etc/nginx/conf.d/OpenSpeedTest-Server-HTTP3.conf
 sed -i "s/{QUIC_CC}/${QUIC_CC}/g" /etc/nginx/conf.d/OpenSpeedTest-Server-HTTP3.conf
 
-echo "HTTP: ${HTTP_PORT} HTTPS: ${HTTPS_PORT} QUIC: ${QUIC_PORT} QUIC_CC ${QUIC_CC}"
+echo "HTTP: ${HTTP_PORT} HTTPS: ${HTTPS_PORT} QUIC: ${QUIC_PORT} QUIC_CC: ${QUIC_CC}"
 
 nginx -g 'daemon off;'
